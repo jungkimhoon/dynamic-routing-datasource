8bf4b51367fbf0db85202617274cd7dfd9618ad7
김정훈 <junghoon.kim@innowireless.com>
2022-07-05 11:22:22 +0900

    add folder

---
+++ src/main/java/com/example/dynamicroutingdatasource/DynamicRoutingDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamicroutingdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicRoutingDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicRoutingDatasourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dynamicroutingdatasource/DynamicRoutingDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamicroutingdatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicRoutingDatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
